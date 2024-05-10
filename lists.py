c44605044bd34cdc34e763abe5d93e3dfe5723ee
Felisoto05 <162723327+Felisoto05@users.noreply.github.com>
2024-05-10 13:31:11 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -12,20 +12,17 @@ def remove_elements(lista):
         else:
             del lista[0]
             return lista
-print(remove_elements(['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']))
 
 def add_elements(list_add_elements):
     list_add_elements.append("Yellow")
     list_add_elements.insert(0,"Pink")
-    return print(list_add_elements)
-add_elements(['Red', 'Green', 'White', 'Black'])
+    return list_add_elements
 
 def is_empty(list_to_check):
     if list_to_check ==[]:
         return True
     else:
         return False
-print(is_empty([]))
 
 def check_lists(list_to_compare1, list_to_compare2):
     if len(list_to_compare1)>=3 and len(list_to_compare2)>=3:
@@ -35,7 +32,6 @@ def check_lists(list_to_compare1, list_to_compare2):
             return False
     else:
         return False
-print(check_lists(['Black', 'Pink', 'Yellow', 'Red', 'Green', 'White'],['Red', 'Green', 'Yellow', 'White', 'Black', 'Pink']))
 
 def lists_of_lists(list_of_lists_to_modify):
     if len(list_of_lists_to_modify[0])>=2 and len(list_of_lists_to_modify[1])>=4 and len(list_of_lists_to_modify[2])>=2:
@@ -43,4 +39,3 @@ def lists_of_lists(list_of_lists_to_modify):
         list_of_lists_to_modify[1]=list_of_lists_to_modify[1][1:4]
         list_of_lists_to_modify[2]=list_of_lists_to_modify[2][-2:]
         return list_of_lists_to_modify
-print(lists_of_lists([[1, 2, 3], [4, 5, 6, 7, 8], [9, 10, 11, 12]]))
